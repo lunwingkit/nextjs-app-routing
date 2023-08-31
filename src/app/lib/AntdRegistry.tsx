511c8908099d292450f3fc90d36880bc9c39f602
Killian Lun <lunwingkit@gmail.com>
2023-08-31 16:29:39 +0800

    no message

--- src/app/lib/AntdRegistry.tsx
+++ src/app/lib/AntdRegistry.tsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs';
 import { useServerInsertedHTML } from 'next/navigation';
+import type Entity from '@ant-design/cssinjs/es/Cache';
 
 const StyledComponentsRegistry = ({ children }: { children: React.ReactNode }) => {
-  const cache = createCache();
+  const cache = React.useMemo<Entity>(() => createCache(), [createCache]);
   useServerInsertedHTML(() => (
     <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }} />
   ));
